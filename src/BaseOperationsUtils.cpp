56da4c06d40737b872f3ddd1f72bc18c2e95fd6e
Moran Shoeg <morantr@gmail.com>
2023-02-18 20:10:25 +0200

    Add the BaseOperationsUtils utility class
    Add a static utility class called BaseOperationsUtils.
    This class contains functions that will help in the calculations of another custom base arithmetic
    operation with the restriction of the project, such as:
    
    - Do the calculations on strings since the permutations could reach values that exceed unsigned
    integer limits.
    
    - The functions need to be fast as possible.
    
    The added functions are:
    1. decimal_to_base_x() - convert a decimal to a new custom base. This function is needed to let some
    worker threads or clients do limited calculations from value x to value y and coordinate the
    calculations between them.
    
    2. sum_base_x_integers() - summarize two strings containing numbers in a custom base.
    This function is also needed for the same reason as the previous function.
    
    3. increment_base_x_integer() - increment a string containing an integer of a custom base by 1.
    This function is needed for iterating on all permutations.
    
    Add unit tests for all functions in the new class.
    
    Signed-off-by: Moran Shoeg <morantr@gmail.com>

---
+++ src/BaseOperationsUtils.cpp
@@ -0,0 +1,85 @@
+#include "BaseOperationsUtils.h"
+
+#include <cmath>
+#include <iostream>
+
+std::string BaseOperationsUtils::decimal_to_base_x(uint decimal, std::string_view base_characters)
+{
+    std::string result;
+
+    do {
+        int digit = decimal % base_characters.size();
+        result += base_characters[digit];
+        decimal /= base_characters.size();
+    } while (decimal > 0);
+
+    std::reverse(result.begin(), result.end());
+    return result;
+}
+
+/**
+ * @note 'Find' operation on a @a std::string_view is faster than doing it on a @a std::map or on a
+ * @a std::unordered_map.
+ * In addition, this function 15 timesfaster than using std::stoi to convert the strings to
+ * integers, sum them, and converting it back to string with std::ostringstream, and 5 times faster
+ * if replacing the std::ostringstream with sprintf.
+ */
+std::string BaseOperationsUtils::sum_base_x_integers(
+    std::string_view int1, std::string_view int2, std::string_view base_characters)
+{
+    const size_t size_max    = std::max(int1.length(), int2.length());
+    const size_t buffer_size = size_max + 2; // placeholder for last carry and null terminator
+
+    constexpr uint8_t max_int_str_size = 20;
+    std::array<char, max_int_str_size> result_buffer;
+
+    result_buffer[buffer_size - 1] = 0; // Assign null terminator
+
+    // Find relative position of each integer.
+    size_t int1_pos = buffer_size - int1.length() - 1;
+    size_t int2_pos = buffer_size - int2.length() - 1;
+
+    uint8_t carry = 0;
+    // Start from 'buffer_size - 2' to eliminate null terminator and size to index shift.
+    for (uint i = buffer_size - 2; i < buffer_size; --i) {
+        uint8_t sum = carry;
+
+        if (i >= int1_pos) {
+            sum += base_characters.find(int1[i - int1_pos]);
+        }
+        if (i >= int2_pos) {
+            sum += base_characters.find(int2[i - int2_pos]);
+        }
+        carry            = sum / base_characters.length();
+        result_buffer[i] = base_characters[sum % base_characters.length()];
+    }
+
+    std::string result;
+    const char *first_char = result_buffer[0] != '0' ? &result_buffer[0] : &result_buffer[1];
+    result.assign(first_char);
+    return result;
+}
+
+void BaseOperationsUtils::increment_base_x_integer(
+    std::string &int_str, std::string_view base_characters)
+{
+    if (int_str.empty()) {
+        int_str.insert(int_str.begin(), base_characters[0]);
+        return;
+    }
+
+    uint8_t carry = 1;
+    for (int i = int_str.length() - 1; i >= 0; --i) {
+        uint8_t sum = base_characters.find(int_str[i]) + carry;
+        carry       = sum / base_characters.length();
+        int_str[i]  = base_characters[sum % base_characters.length()];
+
+        if (!carry) {
+            break;
+        }
+    }
+
+    if (carry) {
+        int_str.insert(int_str.begin(), '1');
+    }
+}
---
+++ src/BaseOperationsUtils.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <string>
+#include <string_view>
+
+class BaseOperationsUtils {
+  public:
+    /**
+     * @brief Convert a @a decimal number to a base with given @a base_characters.
+     * This function is needed since the standard library doesn't provide, a way to convert to any
+     * base.
+     *
+     * @param decimal Decimal number to convert.
+     * @param base_characters List of new base characters.
+     * @return String of the decimal converted to base.
+     *
+     * @example decimal_to_base_x(420, {'0','1',..,'F'}) -> "1A4".
+     */
+    static std::string decimal_to_base_x(uint decimal, std::string_view base_characters);
+
+    /**
+     * @brief Sums two integers in base X, and return the result in base X.
+     *
+     * @param int1 Integer 1 to sum.
+     * @param int2 Integer 2 to sum.
+     * @param base_characters Characters of the base.
+     * @return std::string The sum of the two integers in base X.
+     */
+    static std::string sum_base_x_integers(
+        std::string_view int1, std::string_view int2, std::string_view base_characters);
+
+    /**
+     * @brief Increment integer @a int_str of base X by 1.
+     *
+     * @param int_str Integer to increment.
+     * @param base_characters Characters of the base.
+     */
+    static void increment_base_x_integer(std::string &int_str, std::string_view base_characters);
+};
