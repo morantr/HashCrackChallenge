729a1308594dfc30700ea2b3f822e00d4b02c0a0
Moran Shoeg <morantr@gmail.com>
2023-03-24 23:48:23 +0300

    Changes and refactors UiUtils::build_hash_rate_string().
    The next commit will change the main function to use the
    HashCrackerThread instead of the HashGenerator.
    In the new implementation, the rate of hashes per second will be
    provided instead of counters. This allows simplifying the function.
    
    In addition, extra optimization was done by removing the power
    calculation and using a lookup table for everything, which improves
    performance.
    
    Update the unit test of that function accordingly.
    
    Signed-off-by: Moran Shoeg <morantr@gmail.com>

--- src/UiUtils.cpp
+++ src/UiUtils.cpp
@@ -1,44 +1,37 @@
 #include "UiUtils.h"
 
 #include <array>
-#include <cmath>
 
-std::pair<uint64_t, std::string_view> UiUtils::build_hash_rate_string(
-    uint64_t hash_counter, uint64_t ref_idx, std::chrono::seconds update_rate)
+std::pair<double, std::string_view> UiUtils ::build_hash_rate_string(uint64_t rate)
 {
-    // Calculate the difference between hash_counter and ref_idx
-    uint64_t hash_difference = hash_counter - ref_idx;
-
-    // The constant for converting between units
-    constexpr uint unit_conversion_constant = 1000;
-
     // The units for the hash rate, in increasing order of magnitude
-    constexpr std::array<std::string_view, 4> unit_strings = {
-        " Hash/Sec", " KHash/Sec", " MHash/Sec", " GHash/Sec"};
+    struct sRate {
+        std::string_view rate_str;
+        double rate_factor;
+    };
 
-    // The index of the selected unit
-    int unit_index = 0;
-
-    // Calculate the intermediate value by dividing the hash difference by the update rate
-    uint64_t intermediate_value = hash_difference / update_rate.count();
+    constexpr std::array<sRate, 4> rates = {
+        sRate {" Hash/Sec", 1},          //
+        sRate {" KHash/Sec", 1000},      //
+        sRate {" MHash/Sec", 1000000},   //
+        sRate {" GHash/Sec", 1000000000} //
+    };
 
     // Determine the unit based on the intermediate value
     // The branching here could actually contribute to performance since it is expected to use
     // the same branch all the time.
-    if (intermediate_value >= std::pow(unit_conversion_constant, 3)) {
+    if (rate >= rates[3].rate_factor) {
         // The hash rate is in the giga hashes per second unit
-        unit_index = 3;
-    } else if (intermediate_value >= std::pow(unit_conversion_constant, 2)) {
+        return {rate / rates[3].rate_factor, rates[3].rate_str};
+
+    } else if (rate >= rates[2].rate_factor) {
         // The hash rate is in the mega hashes per second unit
-        unit_index = 2;
-    } else if (intermediate_value >= std::pow(unit_conversion_constant, 1)) {
+        return {rate / rates[2].rate_factor, rates[2].rate_str};
+
+    } else if (rate >= rates[1].rate_factor) {
         // The hash rate is in the kilo hashes per second unit
-        unit_index = 1;
+        return {rate / rates[1].rate_factor, rates[1].rate_str};
     }
 
-    // Calculate the final hash rate
-    uint hash_rate = intermediate_value / std::pow(unit_conversion_constant, unit_index);
-
-    // Return the hash rate, and string representing the its unit
-    return {hash_rate, unit_strings[unit_index]};
+    return {rate / rates[0].rate_factor, rates[0].rate_str};
 }
--- src/UiUtils.h
+++ src/UiUtils.h
@@ -7,16 +7,13 @@
 class UiUtils {
   public:
     /**
-     * @brief Calculate the hash rate between to counters, and build a human readable hash units.
+     * @brief Build a human readable hash rate.
      *
      * @note This function is limmeted to rate of 4 GHash/Sec due to max value of uint64_t.
      *
-     * @param hash_counter Hash counter of the latest hash calculation.
-     * @param ref_idx Reference hash counter.
-     * @param update_rate How many seconds passed between @a hash_counter and @a ref_idx.
+     * @param rate Hash rate per second.
      * @return std::pair<uint64_t, std::string_view> The pair first is the rate numering value, and
      * the pair second is the string representing the unit per seconds.
      */
-    static std::pair<uint64_t, std::string_view> build_hash_rate_string(
-        uint64_t hash_counter, uint64_t ref_idx, std::chrono::seconds update_rate);
+    static std::pair<double, std::string_view> build_hash_rate_string(uint64_t rate);
 };
