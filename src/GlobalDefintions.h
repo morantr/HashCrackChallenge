5985153a2cc5e6d5d0052c3e68539c4585e05902
Moran Shoeg <morantr@gmail.com>
2023-03-26 10:03:38 +0300

    Add GlobalDefintions.h file
    This file contain the salt, pepper and valid characters.
    The same definitions will be removed from the main.cpp file in a future commit.
    
    Signed-off-by: Moran Shoeg <morantr@gmail.com>

---
+++ src/GlobalDefintions.h
@@ -0,0 +1,5 @@
+#include <string_view>
+
+constexpr std::string_view salt        = "IEEE";
+constexpr std::string_view pepper      = "Xtreme";
+constexpr std::string_view valid_chars = "0123456789abcdefghijklmnopqrstuvwxyz";
